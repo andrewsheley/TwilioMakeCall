a164374e1575683319689cee132cac07eb3f204d
Andrew Sheley KDOR <Andrew.Sheley@kdor.ks.gov>
2018-11-15 13:42:04 -0600

    Add project files.

---
+++ TwilioMakeCall/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Twilio;
+using Twilio.Rest.Api.V2010.Account;
+
+
+namespace TwilioMakeCall
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Find your Account Sid and Token at twilio.com/console
+            const string accountSid = "xxxxxxxxxxxxxxxxxxxxx";
+            const string authToken = "xxxxxxxxxxxxxxxxxxxxxx";
+
+            TwilioClient.Init(accountSid, authToken);
+
+            var call = CallResource.Create(
+                url: new Uri("http://demo.twilio.com/docs/voice.xml"),
+                to: new Twilio.Types.PhoneNumber("+xxxxxxxxxxxxxxxxx"),
+                from: new Twilio.Types.PhoneNumber("+xxxxxxxxxxxxxxxx")
+            );
+
+            Console.WriteLine(call.Sid);
+
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
